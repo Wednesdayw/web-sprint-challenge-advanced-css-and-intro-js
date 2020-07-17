511f195a86467e2b835df4254bf88b9b83df210d
Melissa C <66797764+Wednesdayw@users.noreply.github.com>
2020-07-17 13:56:52 -0400

    task 7

--- index.js
+++ index.js
@@ -240,12 +240,18 @@ console.log(getArtistByIndex(artists, [0]));
 
 /* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
 
-// function get20s(/* Code here */){
-
-//   /* Code here */
-
-// }
-
+function get20s(artistArray){
+  let bornIn20thCenturyArray = [];
+  for (i = 0; i < artistArray.length; i++) {
+    let yearBorn = artistArray[i].years.split(" ");
+    yearBorn = yearBorn[0];
+    if (yearBorn >= 1900) {
+      bornIn20thCenturyArray.push(artistArray[i].name);
+    }
+  }
+  return bornIn20thCenturyArray
+}
+console.log(get20s(artists));
 
 /* Task 5: Create a function called `removeArtist` that takes two arguments:
  *     (1) artists array
@@ -257,11 +263,11 @@ console.log(getArtistByIndex(artists, [0]));
  * 
  * Note that sucessfully invoking this function multiple times without refreshing your browser will continuously remove artists from the array until there are none left. If you refresh your browser, the data will reset.  
 */
-// function removeArtist(arr, index) {
-//   arr.splice(index,1);
-//   console.log (arr.length);
-// }
-// removeArtist(artists,0);
+function removeArtist(arr, index) {
+  arr.splice(index,1);
+}
+removeArtist(artists,0);
+console.log (artists);
  
 
 /**
@@ -300,12 +306,17 @@ and returns an array with names of artists who painted more than 100 paintings.
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ..."Albrecht Dürer"]*/
 
-function lotsOfArt(/* Code here */){
-
-  /* Code here */
+function lotsOfArt(arr){
+  const oneHundred=[];
+  for (let i=0; i < arr.length; i++){
+    if (arr[i].paintings>100){
+      oneHundred.push(arr[i]);
+    }
+  }
+return oneHundred;
 
 }
-
+console.log(lotsOfArt(artists));
 
 
 // 🎨🎨 STRETCH 🎨🎨//
